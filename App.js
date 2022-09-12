29229127191b222a4f4f3836f5874bbe41a7e669
gokri20 <gokri20@student.sdu.dk>
2022-09-12 15:27:31 +0200

    modified app.js, added navigation.push()

--- App.js
+++ App.js
@@ -1,32 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
+import {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StatusBar } from 'expo-status-bar';
 import TestFunc from './Test';
 import ForLoops from './Loop';
 import FatList from './Flatlist';
-import { NavigationContainer, StackActions } from '@react-navigation/native';
+import { Button } from 'react-native-web';
 
-export default function App() {
-  return (
-//    <NavigationContainer>
-//      <Stack.Navigator initialRouteName="Home">
-//        <Stack.Screen name="Home" component={HomeScreen} />
-//        <Stack.Screen name="SecondScreen" component={SecondScreen} />
-      <View style={styles.container}>
+function HomeScreen ( {navigation} ) {
+
+    return (
+      <View style={styles.mainContainer}>
         {TestFunc()}
         <StatusBar style="auto" />
         {ForLoops()}
         {FatList()}
+        <Button title="Go to Second Screen" onPress={() => navigation.navigate('SecondScreen')}/>
       </View>
-//      </Stack.Navigator>
-//    </NavigationContainer>
+    );
+  }
+
+  
+
+  function SecondScreen ( {navigation} ) {
+    const [count, setCount] = useState(0);
+
+    function countPlus() {
+      setCount(count+1);
+      navigation.push('SecondScreen');
+      return {count};
+    }
+
+
+    return (
+      <View style={styles.mainContainer}>
+        <Text>Hello {count}</Text>
+        <Button title="Go to Second Screen" onPress={() => countPlus()}/>
+        <Button title="Go to Dat Third Screen" onPress={() => navigation.navigate('DatThirdScreen', {count})}/>
+      </View>
+    );
+  }
+
+  const DatThirdScreen = ({navigation}) => {
+    return (
+      <View>
+          <Text>Third Screen</Text>
+      </View>
+    );
+  }
+
+const Stack = createNativeStackNavigator();
+
+
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="SecondScreen" component={SecondScreen} />
+        <Stack.Screen name="DatThirdScreen" component={DatThirdScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  mainContainer: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+export default App;

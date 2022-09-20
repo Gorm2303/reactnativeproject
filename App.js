d5683d12a79b353e7f1d1c109ae827d47ad31209
gokri20 <gokri20@student.sdu.dk>
2022-09-20 14:31:39 +0200

    reset to normal state

--- App.js
+++ App.js
@@ -3,8 +3,6 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
-import TestFunc from './Test';
-import ForLoops from './Loop';
 import FatList from './Flatlist';
 import { Button } from 'react-native-web';
 
@@ -12,40 +10,23 @@ function HomeScreen ( {navigation} ) {
 
   return (
     <View style={styles.mainContainer}>
-      {TestFunc()}
       <StatusBar style="auto" />
-      {ForLoops()}
       <Button title="Go to Second Screen" onPress={() => navigation.navigate('SecondScreen')}/>
+      <Text>Hello at the first screen</Text>
+      {FatList}
     </View>
   );
 }
 
 function SecondScreen ( {navigation} ) {
-  const [count, setCount] = useState(0);
-
-  function countPlus() {
-    setCount(count+1);
-    navigation.push('SecondScreen');
-    return {count};
-  }
 
   return (
     <View style={styles.mainContainer}>
-      <Text>Hello {count}</Text>
-      <Button title="Go to Second Screen" onPress={() => countPlus()}/>
-      <Button title="Go to Dat Third Screen" onPress={() => navigation.navigate('DatThirdScreen', {count})}/>
+      <Text>Hello at the second screen</Text>
+      <Button title="Go to Second Screen" onPress={() => navigation.push('SecondScreen')}/>
     </View>
   );
 }
-
-const DatThirdScreen = ({navigation}) => {
-  return (
-    <View>
-        <Text>Third Screen</Text>
-        {FatList()}
-    </View>
-  );
-};
 
 const Stack = createNativeStackNavigator();
 function App() {
@@ -54,7 +35,6 @@ function App() {
         <Stack.Navigator initialRouteName="Home">
           <Stack.Screen name="Home" component={HomeScreen} />
           <Stack.Screen name="SecondScreen" component={SecondScreen} />
-          <Stack.Screen name="DatThirdScreen" component={DatThirdScreen} />
         </Stack.Navigator>
     </NavigationContainer>
   );
